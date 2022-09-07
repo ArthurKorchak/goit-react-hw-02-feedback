82fb4be0b104f085fcdb9d63fff2f0348be2f321
ArthurKorchak <korchak.arthur@gmail.com>
2022-09-08 01:21:14 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,66 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
+  buttonsOperator = event => {
+    const target = event.target.name;
+    this.setState(prevState => {
+    return {
+        ...prevState,
+        [target]: prevState[target] + 1,
+      };
+    });
+  };
 
-export const App = () => {
-  return (
-    <>
-      
-    </>
-  );
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, item) => acc + item, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return ((this.state.good / this.countTotalFeedback()) * 100 || 0).toFixed(
+      0
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <p>Please leave feedback</p>
+        <div className="buttonsSet">
+          {Object.keys(this.state).map(item => (
+            <button
+              type="button"
+              name={item}
+              key={item}
+              onClick={this.buttonsOperator}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+        <p>Statistics</p>
+        <div>
+          {Object.keys(this.state).map(item => (
+            <p key={item}>
+              {item}:<span>{this.state[item]}</span>
+            </p>
+          ))}
+          <p>
+            Total:<span>{this.countTotalFeedback()}</span>
+          </p>
+          <p>
+            Positive feedbacks:
+            <span>{this.countPositiveFeedbackPercentage()} %</span>
+          </p>
+        </div>
+      </>
+    );
+  };
 };
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+
+class FeedbackOptions extends Component { };
+
+export default FeedbackOptions;
+
